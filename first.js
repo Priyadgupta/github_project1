272b5df451270f6a92f6223486df1276ed6cd5ff
Priya <priyagupta127@gmail.com>
2022-07-20 11:59:57 +1000

    add multiplication

---
+++ first.js
@@ -0,0 +1,8 @@
+console.log("Hello ");
+
+let num1 = 23;
+let num2 = 34;
+
+let sum = num1 + num2;
+console.log(sum);
+
---
+++ jsfile.js
@@ -0,0 +1,5 @@
+let num1 =10;
+let num2 = 5;
+
+let multi = num1*num2;
+console.log(multi);
